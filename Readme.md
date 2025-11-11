a1dae455a355e8917a2d7d493a7daaa453ff6311
lakshchovatiya <chovatiyalaksh83@gmail.com>
2025-11-11 14:39:28 +0530

    setup project and connect db

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# My First Project : Posts App
+
+- In this app, a user can post private or public content and also can make friends and view their posts.
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+import app from "./app.js";
+
+dotenv.config({ path: "./.env" });
+
+const PORT = process.env.PORT || 8080;
+
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("MONGO connection failed ", err);
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed ", err);
+  });
